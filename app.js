847e0c8fb47582a8c4a3028c5ac77350863d2f0e
Pedro Roig Morera <145972514+PedroRoigM@users.noreply.github.com>
2025-03-01 17:39:56 +0100

    Swagger

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ require('dotenv').config();
 const express = require("express")
 const cors = require("cors")
 const dbConnect = require("./config/mongo")
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const yaml = require('yaml');
 
 const app = express()
+
+const swaggerDocument = yaml.parse(fs.readFileSync('./swagger.yaml', 'utf8'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 //Le decimos a la app de express() que use cors para evitar el error Cross-Domain (XD)
 app.use(cors())
